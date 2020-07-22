63834a42f9001e6046441ff132767c10598ef450
Stefanos <stefanoskokkalhs@gmail.com>
2020-07-22 14:11:26 +0300

    ngMaterialDialog Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
-
+import {MatDialog, MatDialogConfig} from "@angular/material/dialog";
+import { CourseDialogComponent } from './course-dialog/course-dialog.component';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-material-dialog';
+ 
+  constructor(private dialog: MatDialog) {}
+ 
+  openDialog() {
+    
+    const dialogConfig = new MatDialogConfig();
+ 
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+ 
+    dialogConfig.data = {
+        id: 1,
+        title: 'Angular For Beginners'
+    };
+    
+    const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
+ 
+    dialogRef.afterClosed().subscribe(
+        data => console.log("Dialog output:", data)
+    );    
+  }
 }
